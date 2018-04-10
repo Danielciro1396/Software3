476bcd4025ae39064dde76d1d3821bb6d535dcab
Daniel Esteban Ciro Rincon <danielciro1396@hotmail.com>
2018-04-10 09:52:07 -0500

    Extraer datos v2

--- ResearchScraper/src/main/java/co/edu/uniquindio/software3/proyecto/ResearchScraper/CvLac.java
+++ ResearchScraper/src/main/java/co/edu/uniquindio/software3/proyecto/ResearchScraper/CvLac.java
@@ -15,7 +15,6 @@ import org.jsoup.select.Elements;
 public class CvLac {
 
 	public final String url = "http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000438383";
-	// public ArrayList<String> elementos = new ArrayList<String>();
 	public ArrayList<String> elemInfoPersonal = new ArrayList<>();
 	public ArrayList<String> elemFormacionAcam = new ArrayList<>();
 	public ArrayList<String> elemEventos = new ArrayList<>();
@@ -38,35 +37,27 @@ public class CvLac {
 			for (Element elem : entradas) {
 
 				if (elem.text().contains("Nombre en citaciones")) {
-					// nombre += elem.toString() + "\n";
 					elemInfoPersonal.add(elem.toString());
 				}
 				if (elem.text().contains("Formación Académica")) {
-					// nombre += elem.toString() + "\n";
 					elemFormacionAcam.add(elem.toString());
 				}
 				if (elem.text().contains("Eventos científicos")) {
-					// nombre += elem.toString() + "\n";
 					elemEventos.add(elem.toString());
 				}
 				if (elem.text().contains("Artículos")) {
-					// nombre += elem.toString() + "\n";
 					elemArticulos.add(elem.toString());
 				}
 				if (elem.text().contains("Libros")) {
-					// nombre += elem.toString() + "\n";
 					elemLibros.add(elem.toString());
 				}
 				if (elem.text().contains("Informes de investigación")) {
-					// nombre += elem.toString() + "\n";
 					elemInformes.add(elem.toString());
 				}
 				if (elem.text().contains("Proyectos")) {
-					// nombre += elem.toString() + "\n";
 					elemProyectos.add(elem.toString());
 				}
 				if (elem.text().contains("Textos en publicaciones no científicas")) {
-					// nombre += elem.toString() + "\n";
 					elemPublicacionesN.add(elem.toString());
 				}
 
@@ -74,8 +65,9 @@ public class CvLac {
 
 			BufferedWriter writer = null;
 			try {
-				// writer = new BufferedWriter(new FileWriter("d://test.txt"));
-				writer = new BufferedWriter(new FileWriter("C://Users//dan97//Desktop/test.txt"));
+				writer = new BufferedWriter(new FileWriter("d://test.txt"));
+				// writer = new BufferedWriter(new
+				// FileWriter("C://Users//dan97//Desktop/test.txt"));
 				writer.write(nombre);
 				writer.close();
 			} catch (IOException e) {
@@ -83,10 +75,12 @@ public class CvLac {
 				e.printStackTrace();
 			}
 
-			extraerDatos(limpiar(elemInfoPersonal), null, limpiar(elemEventos), null, null, null, null, null);
+			//extraerDatos(limpiar(elemInfoPersonal), null, null, limpiar(elemArticulos), null, null, null, null);
 			// extraerDatos(limpiar(elemInfoPersonal),limpiar(elemFormacionAcam),
 			// limpiar(elemEventos), limpiar(elemArticulos),
 			// limpiar(elemLibros),limpiar(elemInformes),limpiar(elemProyectos),limpiar(elemPublicacionesN));
+
+			 limpiar(elemLibros);
 
 		} else {
 			System.out.println("El Status Code no es OK es: " + getStatusConnectionCode(url));
@@ -99,10 +93,10 @@ public class CvLac {
 	}
 
 	/**
-	 * Método provisto por JSoup para comprobar el Status code de la respuesta que
-	 * recibo al hacer la petición Codigos: 200 OK 300 Multiple Choices 301 Moved
-	 * Permanently 305 Use Proxy 400 Bad Request 403 Forbidden 404 Not Found 500
-	 * Internal Server Error 502 Bad Gateway 503 Service Unavailable
+	 * Método provisto por JSoup para comprobar el Status code de la respuesta
+	 * que recibo al hacer la petición Codigos: 200 OK 300 Multiple Choices 301
+	 * Moved Permanently 305 Use Proxy 400 Bad Request 403 Forbidden 404 Not
+	 * Found 500 Internal Server Error 502 Bad Gateway 503 Service Unavailable
 	 * 
 	 * @param url,
 	 *            el enlace de la página web a analizar.
@@ -121,8 +115,8 @@ public class CvLac {
 	}
 
 	/**
-	 * Método que retorna un objeto de la clase Document con el contenido del HTML
-	 * de la web para poder ser parseado posteriormente con JSoup
+	 * Método que retorna un objeto de la clase Document con el contenido del
+	 * HTML de la web para poder ser parseado posteriormente con JSoup
 	 * 
 	 * @param url,
 	 *            el enlace de la página web a analizar.
@@ -185,6 +179,11 @@ public class CvLac {
 		} catch (NullPointerException e) {
 
 		}
+		try {
+			extraerArticulos(articulos, investigador);
+		} catch (NullPointerException e) {
+
+		}
 
 	}
 
@@ -203,10 +202,10 @@ public class CvLac {
 		String fecha = "";
 		String lugar = "";
 		String rol = "";
-		String aux= "";
-		aux= StringUtils.stripAccents(investigador.getNombre());
-		aux=aux.substring(1);
-		ArrayList <EventoCientifico> eventoAux = new ArrayList<>();
+		String aux = "";
+		aux = StringUtils.stripAccents(investigador.getNombre());
+		aux = aux.substring(1);
+		ArrayList<EventoCientifico> eventoAux = new ArrayList<>();
 		for (int i = 0; i < elementos.size(); i++) {
 			EventoCientifico eventos = new EventoCientifico();
 			if (elementos.get(i).contains("Nombre del evento:")) {
@@ -221,6 +220,10 @@ public class CvLac {
 			if (elementos.get(i).contains("Realizado el:")) {
 				String cadena = elementos.get(i);
 				fecha = cadena.substring(13, 17);
+				if (fecha.contains(",")) {
+					fecha = "No registra";
+				}
+
 				char[] auxiliar = cadena.toCharArray();
 				int posI = 0;
 				int posF = 0;
@@ -238,8 +241,7 @@ public class CvLac {
 					}
 				}
 			}
-			if (elementos.get(i).equalsIgnoreCase(aux)
-					&& elementos.get(i + 1).contains("Rol en el evento:")) {
+			if (elementos.get(i).equalsIgnoreCase(aux) && elementos.get(i + 1).contains("Rol en el evento:")) {
 				rol = elementos.get(i + 2);
 				eventos.setNombre(nombre);
 				eventos.setTipo(tipo);
@@ -247,22 +249,114 @@ public class CvLac {
 				eventos.setFecha(fecha);
 				eventos.setLugar(lugar);
 				eventos.setRol(rol);
-				
-				if (investigador.getEventos()!=null) {
-					eventoAux=investigador.getEventos();
+
+				if (investigador.getEventos() != null) {
+					eventoAux = investigador.getEventos();
 					eventoAux.add(eventos);
-				}else {
+				} else {
 					eventoAux.add(eventos);
 				}
-				
+
 				investigador.setEventos(eventoAux);
 			}
 		}
+		// for (int j = 0; j < investigador.getEventos().size(); j++) {
+		// System.out.println((j+1)+":
+		// "+investigador.getEventos().get(j).getNombre());
+		// System.out.println(investigador.getEventos().get(j).getFecha());
+		// }
 	}
 
-	public void obtenerArticulos() {
-		// TODO
+	public void extraerArticulos(ArrayList<String> elementos, Investigador investigador) {
+		boolean esEspecializada = false;
+		String autores = "";
+		String titulo = "";
+		String lugar = "";
+		String nomRevista = "";
+		String anio = "";
+		String aux = "";
+		aux = StringUtils.stripAccents(investigador.getNombre());
+		aux = aux.substring(1, aux.length() - 1);
+		ArrayList<Articulo> articuloAux = new ArrayList<>();
+		for (int i = 0; i < elementos.size(); i++) {
+			Articulo articulo = new Articulo();
+			if (elementos.get(i).contains("Publicado en revista especializada")) {
+				esEspecializada = true;
+			}
+			if (elementos.get(i).contains(aux.toUpperCase())) {
+				String cadena = elementos.get(i);
+				char[] auxiliar = cadena.toCharArray();
+				int posI = 0;
+				int posF = 0;
+				for (int j = 0; j < auxiliar.length; j++) {
+					if (auxiliar[j] != ' ') {
+						if (auxiliar[j] == '"') {
+							posI = j + 1;
+							for (int k = posI + 1; k < auxiliar.length; k++) {
+								if (auxiliar[k] == '"') {
+									posF = k;
+									titulo = cadena.substring(posI, posF);
+									j = k;
+									break;
+								}
+							}
+						} else if (auxiliar[j] == ':') {
+							posI = j + 1;
+							lugar = cadena.substring(posI);
+						} else {
+							posI = j;
+							for (int k = posI; k < auxiliar.length; k++) {
+								if (auxiliar[k] == '"') {
+									posF = k - 2;
+									autores = cadena.substring(posI, posF);
+									j = k - 1;
+									break;
+								}
+							}
+						}
+					}
+
+				}
+				nomRevista = elementos.get(i + 1);
+			}
+			if (elementos.get(i).contains("ed:")) {
+				nomRevista += " Editorial: " + elementos.get(i + 1);
+			}
+			if (elementos.get(i).contains("fasc.")) {
+				String cadena = elementos.get(i + 1);
+				char[] auxiliar = cadena.toCharArray();
+				int posI = 0;
+				int posF = 0;
+				for (int j = 0; j < auxiliar.length; j++) {
+					if (auxiliar[j] == ',') {
+						posI = j + 1;
+						for (int k = posI; k < auxiliar.length; k++) {
+							if (auxiliar[k] == ',') {
+								posF = k;
+								anio = cadena.substring(posI, posF);
+								j = k;
+								break;
+							}
+						}
+					}
+				}
+				articulo.setEsEspecializada(esEspecializada);
+				articulo.setTitulo(titulo);
+				articulo.setAnio(anio);
+				articulo.setAutores(autores);
+				articulo.setLugar(lugar);
+				articulo.setNomRevista(nomRevista);
+				articuloAux.add(articulo);
+				investigador.setArticulos(articuloAux);
+			}
+
+		}
+//		for (int j = 0; j < investigador.getArticulos().size(); j++) {
+//			System.out.println((j + 1) + ":   " + investigador.getArticulos().get(j).getTitulo());
+//			System.out.println(investigador.getArticulos().get(j).getAnio());
+//		}
 	}
+
 	public int obtenerPosicionArray(ArrayList<String> elementos, String cadena) {
 		int pos = -1;
 		for (int i = 0; i < elementos.size(); i++) {
@@ -321,10 +415,9 @@ public class CvLac {
 
 		}
 		elementosLimpio = aux2;
-		// for (int i = 0; i < elementosLimpio.size(); i++) {
-		// System.out.println(elementosLimpio.get(i));
-		// }
-
+		 for (int i = 0; i < elementosLimpio.size(); i++) {
+		 System.out.println(elementosLimpio.get(i));
+		 }
 		return elementosLimpio;
 	}
 
